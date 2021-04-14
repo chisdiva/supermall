35bdd4356407ce8eda3b1e06c948ea07ded70eed
Kx_Dr <724956394@qq.com>
2021-04-14 19:01:09 +0800

    '添加轮播图组件'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <h2>购物街</h2>
+      </template>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+
 export default {
-name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [{}],
+
+    }
+  },
+  //首页组件创建后发送网络请求
+  created() {
+    //函数返回promise
+    getHomeMultidata().then(res => {
+      //保存数据
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+  }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="home-swiper">
+  <swiper>
+    <swiper-item v-for="(item, id) in banners" :key="id">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</div>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from '@/components/common/swiper/index'
+
+export default {
+  name: "HomeSwiper",
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  props: {
+    //从父组件获取轮播图数据
+    banners: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
